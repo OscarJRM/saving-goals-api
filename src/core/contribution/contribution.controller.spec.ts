0a6ab9f7adf86809e2a5c3935301aa778694f364
sebas <sebas.herrera3152@gmail.com>
2025-04-24 14:35:31 -0500

    adding specs to contribution

--- src/core/contribution/contribution.controller.spec.ts
+++ src/core/contribution/contribution.controller.spec.ts
@@ -1,20 +1,20 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ContributionController } from './contribution.controller';
-import { ContributionService } from './contribution.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { ContributionController } from './contribution.controller'
+import { ContributionService } from './contribution.service'
 
 describe('ContributionController', () => {
-  let controller: ContributionController;
+  let controller: ContributionController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ContributionController],
       providers: [ContributionService],
-    }).compile();
+    }).compile()
 
-    controller = module.get<ContributionController>(ContributionController);
-  });
+    controller = module.get<ContributionController>(ContributionController)
+  })
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+    expect(controller).toBeDefined()
+  })
+})
--- src/core/contribution/contribution.service.spec.ts
+++ src/core/contribution/contribution.service.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ContributionService } from './contribution.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { ContributionService } from './contribution.service'
 
 describe('ContributionService', () => {
-  let service: ContributionService;
+  let service: ContributionService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [ContributionService],
-    }).compile();
+    }).compile()
 
-    service = module.get<ContributionService>(ContributionService);
-  });
+    service = module.get<ContributionService>(ContributionService)
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+})
