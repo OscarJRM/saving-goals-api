1823b00b1f7b61f21a6dd4af422f70df925e04e2
Daniel Zhu <dzhu2409@gmail.com>
2025-04-24 12:38:25 -0500

    Add admin prisma model wiith auth

--- src/core/auth/auth.service.ts
+++ src/core/auth/auth.service.ts
@@ -6,6 +6,7 @@ import { DisplayableException } from 'src/common/exceptions/displayable.exceptio
 import { comparePassword, hashPassword } from 'src/common/utils/encrypter'
 import { PrismaService } from 'src/global/prisma/prisma.service'
 import { SignUpDto } from './dto/sign-up.dto'
+import { Admin, User } from '@prisma/client'
 
 @Injectable()
 export class AuthService {
@@ -16,28 +17,35 @@ export class AuthService {
 
   async signIn({ email, password }: SignInDto) {
     const user = await this.prismaService.user.findUnique({
-      where: {
-        email,
-      },
+      where: { email },
     })
 
-    if (!user)
-      throw new DisplayableException(
-        'Usuario no encontrado',
-        HttpStatus.NOT_FOUND,
-      )
+    if (!user) {
+      const admin = await this.prismaService.admin.findUnique({
+        where: { email },
+      })
 
-    const isPasswordValid = comparePassword(password, user.passwordHash)
+      if (!admin) {
+        throw new DisplayableException(
+          'Usuario no encontrado',
+          HttpStatus.NOT_FOUND,
+        )
+      }
 
-    if (!isPasswordValid)
-      throw new DisplayableException(
-        'Creedenciales incorrectas',
-        HttpStatus.BAD_REQUEST,
-      )
+      this.verifyPassword(password, admin.password)
 
-    return {
-      token: this.createToken({ id: user.id }),
+      return this.createToken({
+        id: admin.id,
+        isAdmin: true,
+      })
     }
+
+    this.verifyPassword(password, user.passwordHash)
+
+    return this.createToken({
+      id: user.id,
+      isAdmin: false,
+    })
   }
 
   async signUp(dto: SignUpDto) {
@@ -65,5 +73,47 @@ export class AuthService {
 
   private createToken = (payload: IJwtPayload) => {
     return this.jwtService.sign(payload)
+  }
+
+  async isAdmin(email: string): Promise<boolean> {
+    const admin = await this.prismaService.admin.findUnique({
+      where: { email },
+    })
+    return !!admin
+  }
+
+  async validateUser(
+    id: number,
+    isAdmin: boolean,
+  ): Promise<User | Admin | null> {
+    if (isAdmin) {
+      return await this.prismaService.admin.findUnique({ where: { id } })
+    }
+    return await this.prismaService.user.findUnique({
+      where: {
+        id,
+      },
+    })
+  }
+
+  private verifyPassword(password: string, userPassword: string) {
+    const isPasswordValid = comparePassword(password, userPassword)
+
+    if (!isPasswordValid)
+      throw new DisplayableException(
+        'Creedenciales incorrectas',
+        HttpStatus.BAD_REQUEST,
+      )
+
+    return isPasswordValid
+  }
+
+  verifyToken = (token: string) => {
+    try {
+      return this.jwtService.verify(token)
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+      throw new DisplayableException('Token inválido', HttpStatus.UNAUTHORIZED)
+    }
   }
 }
--- src/core/auth/types/jwt-payload.interface.ts
+++ src/core/auth/types/jwt-payload.interface.ts
@@ -1,3 +1,4 @@
 export interface IJwtPayload {
   id: number
+  isAdmin: boolean
 }
